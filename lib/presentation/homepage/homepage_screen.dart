e8849ada860fa7ca18ea869911dfd4eec4fb154d
im-yejun <yejun_1220@naver.com>
2022-09-07 23:17:55 +0900

    [Insert] 기본 홈페이지 화면 UI 작성

---
+++ lib/presentation/homepage/homepage_screen.dart
@@ -0,0 +1,31 @@
+import 'package:dating_app/presentation/homepage/homepage_event.dart';
+import 'package:dating_app/presentation/homepage/homepage_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<HomePageViewModel>();
+    final state = viewModel.state;
+
+    return Scaffold(
+      body: state.pageOptions[state.index],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.departure_board), label: "FreeBoard")
+        ],
+
+        currentIndex: state.index,
+        selectedItemColor: Colors.blue,
+
+        onTap: (index) {
+          viewModel.onEvent(HomePageEvent.selectCurrentPage(index));
+        },
+      ),
+    );
+  }
+}
