577eced3c0fbe66dea19506f6091a10fb7395e17
im-yejun <yejun_1220@naver.com>
2022-09-10 22:32:04 +0900

    [Insert] 자유게시판 BottomSheet UI 구현 및 게시글 수정하기 구현

--- lib/presentation/freeboard/freeboard_info/freeboard_info_screen.dart
+++ lib/presentation/freeboard/freeboard_info/freeboard_info_screen.dart
@@ -33,9 +33,80 @@ class _FreeBoardInfoScreenState extends State<FreeBoardInfoScreen> {
     final viewModel = context.watch<FreeBoardInfoViewModel>();
     final state = viewModel.state;
 
+    void _handleFABPressed() {
+      showModalBottomSheet(
+        backgroundColor: Colors.transparent,
+        context: context,
+        builder: (context) {
+          return SizedBox(
+            height: 270,
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(8.0),
+                  clipBehavior: Clip.antiAlias,
+                  decoration: const BoxDecoration(
+                    color: Colors.white60,
+                    borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text('Article menu'),
+                      ),
+                      const Divider(),
+                      ListTile(
+                        title: const Text('Edit'),
+                          onTap: () async {
+                            final repository = context.read<FreeBoardRepositoryImpl>();
+
+                            bool? isSaved = await Navigator.of(context, rootNavigator: true).push(
+                              MaterialPageRoute(
+                                builder: (_) => ChangeNotifierProvider(
+                                  create: (_) => PostEditFreeBoardViewModel(repository, state.freeBoardInfo!),
+                                  child: const PostEditFreeBoardScreen(),
+                                ),
+                              ),
+                            );
+
+                            if (isSaved != null && isSaved) {
+                              viewModel.onEvent(FreeBoardInfoEvent.loadFreeBoardInfo(state.freeBoardInfo!.id!));
+                            }
+                          },
+                      ),
+                      const Divider(),
+                      const ListTile(
+                        title: Text('Remove'),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(8.0),
+                  clipBehavior: Clip.antiAlias,
+                  decoration: const BoxDecoration(
+                    color: Colors.white60,
+                    borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                  ),
+                  child: ListTile(
+                    title: Text('CANCEL'),
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      );
+    }
+
     return GestureDetector(
       onTap: () {
-        FocusScope.of(context).unfocus(); // 여백 화면 탭시 키보드 사라짐
+        FocusScope.of(context).unfocus();
       },
       child: Scaffold(
         resizeToAvoidBottomInset: true,
@@ -49,29 +120,8 @@ class _FreeBoardInfoScreenState extends State<FreeBoardInfoScreen> {
             ),
           ),
           actions: [
+            IconButton(onPressed: _handleFABPressed, icon: const Icon(Icons.menu)),
             // TODO : 팝업 창으로 수정 및 삭제 Event 주입
-            IconButton(
-              icon: const Icon(
-                Icons.edit,
-                color: Colors.black,
-              ),
-              onPressed: () async {
-                final repository = context.read<FreeBoardRepositoryImpl>();
-
-                bool? isSaved = await Navigator.of(context, rootNavigator: true).push(
-                  MaterialPageRoute(
-                    builder: (_) => ChangeNotifierProvider(
-                      create: (_) => PostEditFreeBoardViewModel(repository, state.freeBoardInfo!),
-                      child: const PostEditFreeBoardScreen(),
-                    ),
-                  ),
-                );
-
-                if (isSaved != null && isSaved) {
-                  viewModel.onEvent(FreeBoardInfoEvent.loadFreeBoardInfo(state.freeBoardInfo!.id!));
-                }
-              },
-            ),
           ],
           backgroundColor: const Color(0xffffffff),
           iconTheme: const IconThemeData(
